bcfa1ac18cd69f0be941b6bf1c6c49e8257f6dba
ashmisikha piri <149249477+Ashmisikha@users.noreply.github.com>
2025-02-21 22:20:59 +0530

    Create transactions.h

---
+++ transactions.h
@@ -0,0 +1,8 @@
+#ifndef TRANSACTIONS_H
+#define TRANSACTIONS_H
+
+void deposit(int account_number, float amount);
+void withdraw(int account_number, float amount);
+void view_transactions(int account_number);
+
+#endif
